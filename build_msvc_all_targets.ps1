e5631502fc26b34b966ba93eca4222ba271311cb
LT-Schmiddy <schmidalex1996@gmail.com>
2025-03-10 19:49:09 -0400

    refined mips only build.

---
+++ build_msvc_all_targets.ps1
@@ -0,0 +1,21 @@
+﻿# cd llvm-project/llvm;
+
+$source_dir = "./llvm-project/llvm";
+$build_dir = "./build";
+
+Set-Variable CC=cl;
+Set-Variable CXX=cl;
+
+cmake `
+	-S $source_dir `
+	-B $build_dir `
+	-G Ninja `
+	-DCMAKE_BUILD_TYPE=Release `
+	-DLLVM_DEFAULT_TARGET_TRIPLE=host `
+	-DLLVM_TARGETS_TO_BUILD=Mips `
+	-DLLVM_ENABLE_PROJECTS="clang;lld" ;
+	-DLLVM_ENABLE_PROJECTS="clang;lld;lldb;clang-tools-extra" ;
+	-DLLVM_ENABLE_RUNTIMES="libc;libcxxabi;libcxx" `
+# ;
+	
+cmake --build $build_dir;
