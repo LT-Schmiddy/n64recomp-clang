29d5a98534d846acf436158d831fdd2941b42f28
LT-Schmiddy <schmidalex1996@gmail.com>
2025-03-10 16:53:45 -0400

    fixing build script typo

--- build_msvc_mips_only.ps1
+++ build_msvc_mips_only.ps1
@@ -9,11 +9,12 @@ Set-Variable CXX=cl;
 cmake `
 	-S $source_dir `
 	-B $build_dir `
-	-G Ninja; `
+	-G Ninja `
 	-DCMAKE_BUILD_TYPE=Release `
 	-DLLVM_DEFAULT_TARGET_TRIPLE=mips `
 	-DLLVM_TARGETS_TO_BUILD=Mips `
 	-DLLVM_ENABLE_PROJECTS="clang;lld;lldb;clang-tools-extra" `
-	-LLVM_ENABLE_RUNTIMES="libc;libcxx;libcxxabi" `
+	-DLLVM_ENABLE_RUNTIMES="libc;libcxxabi;libcxx" `
+;
 	
 cmake --build $build_dir;
