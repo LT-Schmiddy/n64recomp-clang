9cfffc4256fb8a33bf0f91bfc45ee2cb6b8f43d1
LT-Schmiddy <schmidalex1996@gmail.com>
2025-03-24 20:26:59 -0400

    Refining Build Script

--- mips_only_build.ps1
+++ mips_only_build.ps1
@@ -1,5 +1,12 @@
 ﻿# cd llvm-project/llvm;
 
+$drive = Get-Location;
+Write-Output $drive;
+
+Launch-VsDevShell;
+
+Set-Location $drive
+
 $source_dir = "./llvm-project/llvm";
 $build_dir = "./build";
 
@@ -13,7 +20,6 @@ cmake `
 	-DCMAKE_BUILD_TYPE=Release `
 	-DLLVM_DEFAULT_TARGET_TRIPLE=mips `
 	-DLLVM_TARGETS_TO_BUILD=Mips `
-	-DLLVM_ENABLE_PROJECTS="clang;lld;clang-tools-extra" `
+	-DLLVM_ENABLE_PROJECTS="clang;lld;" `
 ;
-	
 cmake --build $build_dir;
