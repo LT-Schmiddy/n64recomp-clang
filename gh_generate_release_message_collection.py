1b7cdf88192e039770a0e203378ebd8e166db1cb
LT-Schmiddy <schmidalex1996@gmail.com>
2025-12-04 21:01:45 -0500

    f*ck it. Skip the clang and shim versions for now

--- gh_generate_release_message_collection.py
+++ gh_generate_release_message_collection.py
@@ -8,12 +8,8 @@ import build_llvm
 release_md = build_llvm.proot.joinpath("_release.md")
 
 shim_version_str = build_shim_tool.get_shim_version_string()
-if (len(sys.argv) > 1):
-    shim_version_str = sys.argv[1]
 
 clang_version_str = build_llvm.get_clang_version_string().title()
-if (len(sys.argv) > 2):
-    clang_version_str = sys.argv[2]
 
 release_md.write_text(f"""
 
