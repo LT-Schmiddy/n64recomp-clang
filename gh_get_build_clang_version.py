455f9520ea3fd7df3aa2b14720bcdd809e916ed6
LT-Schmiddy <schmidalex1996@gmail.com>
2026-03-24 17:02:52 -0400

    no longer using these

--- gh_get_build_clang_version.py
+++
@@ -1,7 +0,0 @@
-import sys, os, shutil, subprocess, platform
-from pathlib import Path
-
-import build_llvm as bl
-
-clang_str = bl.get_clang_version_string().title()
-print(f"generated_value=\"{clang_str}\"")
--- gh_get_build_shim_version.py
+++
@@ -1,7 +0,0 @@
-import sys, os, shutil, subprocess, platform
-from pathlib import Path
-
-import build_shim_tool as bs
-
-shim_str = bs.get_shim_version_string()
-print(f"generated_value=\"{shim_str}\"")
